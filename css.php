88a7c7e3643d59acade166789a93d8c2d2eb8872
BABYLONTHEMAN <70661469+BABYLONTHEMAN@users.noreply.github.com>
2023-03-27 06:18:24 -0700

    Animation using CSS

---
+++ css.php
@@ -0,0 +1,60 @@
+<?php 
+<style> 
+
+
+body{
+    background-color: rgb(126, 160, 165);
+}
+    div {
+        background-color: #715af3;
+        width: 151px;
+        height: 100px;
+        animation-name: example;
+        animation-duration: 2s;
+        animation-direction: reverse;
+        animation-iteration-count: 4;
+    }
+    
+    @keyframes example {
+        from {background-color: rgb(228, 199, 199); width: 151px;}
+        to {background-color: rgb(61, 218, 61);  width: 11px;}
+    }
+
+
+    .a {
+        
+        width: 151px;
+        height: 100px;
+        background-color: rgb(228, 199, 199);
+        animation-name: example;
+        animation-duration: 2s;
+        animation-direction: reverse;
+        animation-iteration-count: 4;
+    }
+    
+    @keyframes example {
+        from {background-color: rgb(228, 199, 199); width: 151px;}
+        to {background-color: rgb(123, 199, 123);  width: 11px;}
+    }
+    ba
+    </style>
+    
+    <div>
+        this div for simulation
+        
+        </div>
+        <br>
+        
+        <div>
+            other div
+            
+         </div> 
+         
+         <br>
+        
+         <p class="a">
+             3rd div
+             
+         </p>
+
+         ?>
