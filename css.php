fedf04fef75f12ed751366c35cb77249e47224b6
BABYLONTHEMAN <70661469+BABYLONTHEMAN@users.noreply.github.com>
2023-03-27 09:03:47 -0700

    Animated transform and  movement

--- css.php
+++ css.php
@@ -10,33 +10,101 @@ body{
         width: 151px;
         height: 100px;
         animation-name: example;
-        animation-duration: 2s;
-        animation-direction: reverse;
-        animation-iteration-count: 4;
+        animation-duration: 1s;
+        animation-direction: alternate ;
+        animation-iteration-count: infinite;
+        border-radius:  30px;
+        box-shadow: 0 0 4px #999;
+        padding: 16px 32px 32px 32px;
     }
     
     @keyframes example {
-        from {background-color: rgb(228, 199, 199); width: 151px;}
-        to {background-color: rgb(61, 218, 61);  width: 11px;}
+        from {color: #715af3; padding: 16px 32px 32px 32px;}
+        to   { color: #999;   padding: 32px 32px 32px 32px;}
     }
+    
 
 
-    .a {
-        
+    .a,div {
+        padding: 22px 10px;
         width: 151px;
         height: 100px;
-        background-color: rgb(228, 199, 199);
-        animation-name: example;
-        animation-duration: 2s;
-        animation-direction: reverse;
-        animation-iteration-count: 4;
+        filter: grayscale(5%);
+        animation-name: example ;
+        animation-duration: 2.5s;
+        animation-iteration-count: infinite;
+        border-radius:  30px;
+        padding: 16px 32px 32px 32px;
+        
+        font-family:'Source Sans Pro',sans-serif;
+  border-radius:5px;
+  color:#b01a1a;
+  padding-top:25px;
+  margin-left:auto;
+  margin-right:auto;
+  max-width:230px;
+  background-color:#282A35
+  border-radius:  30px;
     }
     
     @keyframes example {
-        from {background-color: rgb(228, 199, 199); width: 151px;}
-        to {background-color: rgb(123, 199, 123);  width: 11px;}
+        
+        from {color: #715af3; padding: 16px 32px 32px 32px;}
+        to   { color: #999;   padding: 32px 32px 32px 32px;}
+        0% {transform: rotate(0deg);left:0px;}
+25% {transform: rotate(20deg);left:0px;}
+50% {transform: rotate(0deg);left:500px;}
+55% {transform: rotate(0deg);left:500px;}
+70% {transform: rotate(0deg);left:500px;background:#1ec7e6;}
+100% {transform: rotate(-360deg);left:0px;}
     }
-    ba
+    
+
+    .b {
+
+        width: 151px;
+        height: 100px;
+        animation: b 8s infinite;
+        padding:0px;
+        border-radius:50px;
+        
+        font-family:'Source Sans Pro',sans-serif;
+  border-radius:5px;
+  color:#b01a1a;
+  margin-left:auto;
+  margin-right:auto;
+  max-width:230px;
+  background-color:yellowgreen;
+  position: relative;
+text-align: center;
+    }
+    
+    @keyframes b {
+    
+        0% {transform: rotate(0deg);left:0px;}
+25% {transform: rotate(20deg);left:0px;}
+50% {transform: rotate(0deg);left:500px;}
+55% {transform: rotate(0deg);left:500px;}
+70% {transform: rotate(0deg);left:500px;background:#1ec7e6;}
+100% {transform: rotate(-360deg);left:0px;}
+
+from {
+        left: -300px;
+        border-radius:20px;
+        text-align: center;
+
+    }
+    to {
+        border-radius:20px;
+        text-align: center;
+
+        left: -300px;
+    }
+    
+    }
+    
+  
+
     </style>
     
     <div>
@@ -52,9 +120,14 @@ body{
          
          <br>
         
-         <p class="a">
-             3rd div
-             
-         </p>
+         <form class="a">
+         @keyframes example             
+</form>
+<p class="b"> 
+<br>
+<br>
 
+position: relative;
+
+</p>
         <?php ?>
